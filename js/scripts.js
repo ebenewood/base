bec08eba6a542280fc984853911d80f2038c8845
eben <student@epicodus.com>
2016-03-07 22:21:05 -0800

    creates logic for binary to decimal, does not allow for user interface

---
+++ js/scripts.js
@@ -0,0 +1,95 @@
+// =============
+
+// function arrMake (userString) {
+//   return userString.String('');
+// };
+
+// function binDec (arrayVar) {
+//   val = 0;
+//     for (i = 0; i <= arrayVar.length; i++) {
+//       alert('this is i ' + i);
+//       a = parseInt(arrayVar[i]);
+//       alert('this is a ' + a);
+//       val = val += i * i + a;
+//       alert('this is val ' + val);
+//     };
+//   return val;
+// };
+
+function stringMake (userString) {
+  stringMade = String(userString);
+  stringSplit = stringMade.split('');
+  numArray = [];
+    for (i = 0; i <= (stringSplit.length - 1); i++) {
+      a = parseInt(stringSplit[i]);
+      numArray.unshift(a);
+  };
+  return numArray;
+};
+
+function binDec (arrayVar) {
+  a = 0;
+  i = 0;
+  for (i = 0; i <= (arrayVar.length - 1); i++) {
+      if (arrayVar[i] != 0) {
+      a += Math.pow(2,i);
+    };
+  };
+  return a;
+};
+
+// ==========
+
+// $(function () {
+//   var a = binDec(stringMake($('input#num').val));
+//   $('span').text(a);
+// });
+
+$(function () {
+
+  var input = $('#userInput').val();
+  var stringResult = stringMake(input).val();
+  var numResult = binDec(stringResult).val();
+
+  $('form').submit(function () {
+    $('#result').text(numResult);
+  });
+  preventDefault.event();
+});
+
+// $(function () {
+//
+//   var input = $("#phrase").val();
+//   var result = namedFunction(input);
+//
+//
+//
+//   $("#answer").text(result);
+//
+// });
+
+
+//
+// function binDec (arrayVar) {
+//   for (i = 0; i <= (arrayVar.length - 1); i++) {
+//       a = i;
+//       b = i;
+//       if ((arrayVar[i]) != 0) {
+//         for (j = a; j <= 0; j--) {
+//           b *= b;
+//         };
+//       };
+//   };
+// };
+
+// function binDec (arrayVar) {
+//   for (i = 0; i <= (arrayVar.length - 1); i ++) {
+//     a = i;
+//     b = i;
+//     do {
+//       a *= a;
+//       b --;
+//       console.log(a);
+//     } while (b >= 0);
+//   };
+// };
